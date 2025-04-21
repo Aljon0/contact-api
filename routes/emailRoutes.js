3e4ff8f2fddc6ad1f86e94ec294c9d4469bcfea4
Aljon0 <aljons702@gmail.com>
2025-04-21 10:25:35 +0800

    Remove deprecated configuration, email, and chatbot functionalities; consolidate email sending and chatbot logic into server.js

--- routes/emailRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { sendEmailHandler } from "../controllers/emailController.js";
-
-const router = express.Router();
-
-router.post("/api/send-email", sendEmailHandler);
-
-export default router;
